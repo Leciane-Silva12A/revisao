f147676be80c6470420556f2b8a5deaf66b0e689
Leciane Leonardo da Silva <leciane.leonardo@eaportal.org>
2026-08-20 08:33:13 -0300

    acabou?

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,5 @@
 async function buscarFilmes() {
-    // através do acesso a rota GET, trazer os filmes e mostrar na tela
-    const resposta = await fetch("http://localhost:3333")
+    const resposta = await fetch("https://backendfilmes.vercel.app/")
     const filmes = await resposta.json()
     const sectionFilmes = document.querySelector(".filmes")
     
