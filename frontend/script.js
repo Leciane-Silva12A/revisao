c0da0518316c646ca72868109bde3ef5aca9d8f2
Leciane Leonardo da Silva <leciane.leonardo@eaportal.org>
2026-09-03 08:20:34 -0300

    cadastro e algumas outras coisas

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,6 @@
 async function buscarFilmes() {
-    const resposta = await fetch("https://backendfilmes.vercel.app/")
+    // através do acesso a rota GET, trazer os filmes e mostrar na tela
+    const resposta = await fetch("https://backend-filmes03mc.vercel.app/")
     const filmes = await resposta.json()
     const sectionFilmes = document.querySelector(".filmes")
     
@@ -10,9 +11,20 @@ async function buscarFilmes() {
                 <p><strong>Gênero:</strong> ${filme.gender}</p>
                 <p><strong>Duração:</strong> ${filme.duration} minutos</p>
                 <p><strong>Classificação indicativa:</strong> ${filme.ageLimit > 0 ? filme.ageLimit + ' anos' : 'Livre'}</p>
+
+                <button onclick="apagarFilme(${filme.id})">Apagar</button>
             </div>
         `
     })
+}
+
+async function apagarFilme(id) {
+    const resposta = await fetch(`https://backend-filmes03mc.vercel.app/delete/${id}`, { method: "DELETE" })
+    const respostaJS = await resposta.json()
+
+    alert(respostaJS.message)
+
+    window.location.reload()
 }
 
 buscarFilmes()
