a5d66abe5d31b46ceec349086aef59c209c191d8
Leciane Leonardo da Silva <leciane.leonardo@eaportal.org>
2026-09-09 12:17:09 -0300

    18

--- frontend/script.js
+++ frontend/script.js
@@ -1,29 +1,38 @@
 async function buscarFilmes() {
-    const resposta = await fetch("https://backendfilmes.vercel.app/")
-    const filmes = await resposta.json()
-    const sectionFilmes = document.querySelector(".filmes")
-    
-    filmes.forEach((filme) => {
-        sectionFilmes.innerHTML += `
-            <div>
-                <h2>${filme.title}</h2>
-                <p><strong>Gênero:</strong> ${filme.gender}</p>
-                <p><strong>Duração:</strong> ${filme.duration} minutos</p>
-                <p><strong>Classificação indicativa:</strong> ${filme.ageLimit > 0 ? filme.ageLimit + ' anos' : 'Livre'}</p>
+    try {
+        const resposta = await fetch("https://backendfilmes.vercel.app/")
+        const filmes = await resposta.json()
+        const sectionFilmes = document.querySelector(".filmes")
+        
+        sectionFilmes.innerHTML = ""
 
-                <button onclick="apagarFilme(${filme.id})">Apagar</button>
-            </div>
-        `
-    })
+        filmes.forEach((filme) => {
+            sectionFilmes.innerHTML += `
+                <div>
+                    <h2>${filme.title}</h2>
+                    <p><strong>Gênero:</strong> ${filme.gender}</p>
+                    <p><strong>Duração:</strong> ${filme.duration} minutos</p>
+                    <p><strong>Classificação indicativa:</strong> ${filme.ageLimit}</p>
+
+                    <button onclick="apagarFilme(${filme.id})">Apagar</button>
+                </div>
+            `
+        })
+    } catch (erro) {
+        console.error("Erro ao buscar filmes:", erro)
+    }
 }
 
 async function apagarFilme(id) {
-    const resposta = await fetch(`https://backendfilmes.vercel.app/delete/${id}`, { method: "DELETE" })
-    const respostaJS = await resposta.json()
+    try {
+        const resposta = await fetch(`https://backendfilmes.vercel.app/delete/${id}`, { method: "DELETE" })
+        const respostaJS = await resposta.json()
 
-    alert(respostaJS.message)
-
-    window.location.reload()
+        alert(respostaJS.message)
+        window.location.reload()
+    } catch (erro) {
+        console.error("Erro ao apagar filme:", erro)
+    }
 }
 
 buscarFilmes()
